66cfa2628434f81631a28d4c27b1d797efedf6a5
Yousuff Fahim <83778673+Yousuff-Fahim@users.noreply.github.com>
2022-03-05 08:35:42 +0530

    As of 5th March 8:30am
    1) Made the game plan
    2) Made all the sprites/assets and kept them ready (Audio, Sprites, Font etc..)
    3) Made the base of Level 1, 2 & 3
    4) Created an outer environment after level 3
    5) Added the background music
    6) Added animation to the player
    7) Wrote a player movement &  player animation script
    8) Made a camera follow script (Camera will follow the player)
    9) Added a sound for player's jumps
    10) Made the Main Menu, Mission complete menu, Pause Menu and Level Info Menu
    11) Coded a 'button controller', 'mission complete', 'pause menu' and 'level info' menu scripts
    
    This includes all the tasks done in March 1st, 2nd, 3rd and 4th
    How the game looks so far - https://youtu.be/E2B_RGZ6e4I

--- Assets/Scripts/Menu Scripts/BtnConotroller.cs
+++ Assets/Scripts/Menu Scripts/BtnConotroller.cs
@@ -1,36 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BtnConotroller : MonoBehaviour
 {
     public void Play()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Level Select");
-    }
-
-    public void Controls()
-    {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Controls");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level 1");
     }
 
     public void Quit()
     {
         Application.Quit();
-    }
-
-    public void Level1()
-    {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Level 1");
-    }
-
-    public void Level2()
-    {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Level 2");
-    }
-
-    public void Level3()
-    {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Level 3");
     }
 }
---
+++ Assets/Scripts/Menu Scripts/LevelInfoScripts.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class LevelInfoScripts : MonoBehaviour
+{
+    public GameObject LevelInfoUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LevelInfoUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            LevelInfoUI.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+}
