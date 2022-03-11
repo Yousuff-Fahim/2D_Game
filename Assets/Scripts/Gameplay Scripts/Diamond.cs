371eb714619d539bdfd5ed29be19cf2d5973039a
Yousuff Fahim <83778673+Yousuff-Fahim@users.noreply.github.com>
2022-03-11 08:26:12 +0530

    As of 8:30AM 11th March 2022
    1) Added Platforms
    2) Added enemies
    3) Animated enemies
    4) Wrote C# scripts for the enemies to work as required
    5) Added obstacles
    6) Animated obstacles
    7) Wrote C# scripts for the obstacles to work as required
    8) Added Door and exit point to complete the level
    9) Made Game Over menu which restarts the level automatically
    10) Made a level complete menu which will start the next level automatically
    11) Wrote C# scripts to trigger the Game Over and Level complete menu as required
    12) Finalized Level 1, 2 & 3
    
    This includes all the tasks done in March 5th, 6th, 7th, 8th, 9th and 10th
    How the game looks so far - https://youtu.be/ufxBQNHmQC4

---
+++ Assets/Scripts/Gameplay Scripts/Diamond.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Diamond : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Door.instance.RegisterDiamond();    
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.CompareTag(TagManager.PLAYER_TAG))
+        {
+            SoundController.instance.Play_CollectableSound();
+            Door.instance.DiamondCollected();
+            gameObject.SetActive(false);
+        }
+
+    }
+
+}
--- Assets/Scripts/Menu Scripts/LevelInfoScripts.cs
+++ Assets/Scripts/Menu Scripts/LevelInfoScripts.cs
@@ -7,17 +7,17 @@ public class LevelInfoScripts : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        LevelInfoUI.SetActive(true);
-        Time.timeScale = 0f;
+        //LevelInfoUI.SetActive(true);
+        //Time.timeScale = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            LevelInfoUI.SetActive(false);
-            Time.timeScale = 1f;
-        }
+        //if (Input.GetKeyDown(KeyCode.Return))
+        //{
+            //LevelInfoUI.SetActive(false);
+            //Time.timeScale = 1f;
+        //}
     }
 }
